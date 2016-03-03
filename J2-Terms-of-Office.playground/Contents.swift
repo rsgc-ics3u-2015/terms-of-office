8d015d2f8066525ce64254ec9bc203973ac4827f
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-03 12:38:22 -0500

    Basic solution complete
    Purposely not making use of built-in math functions… demonstrate
    knowledge of type conversions

---
+++ J2-Terms-of-Office.playground/Contents.swift
@@ -0,0 +1,89 @@
+// Russell Gordon
+// J2 - Terms of Office
+
+// Notes
+//
+// The current year is always a year when all positions were changed over.
+//
+// Any year beyond x, the current year, would have to have 2, 4, 3, and 5 as factors.
+//
+// What number is that?
+// 
+// Multiples of 2:
+// 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60...
+//
+// Multiples of 4:
+// 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60...
+//
+// Multiples of 3:
+// 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60...
+//
+// Multiples of 5:
+// 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60...
+//
+// So the lowest common multiple of 2, 4, 3, and 5 is 60.
+//
+// Any year that is has 60 as a divisor with no remainder is an "unusual" year where all positions change over.
+
+
+
+// Inputs
+//
+// c    - the current year, an integer
+// f    - the future year, an integer, greater than the current year
+
+// Test cases
+
+// 1: A single tile
+let c : String? = "2016"
+let f : String? = "2100"
+
+
+// Number of special years
+var specialYears : Int = 0
+
+// Only continue when input has been provided for both variables
+if let givenC = c, givenF = f {
+    
+    // Convert given inputs to integer
+    if let currentYear = Int(givenC), futureYear = Int(givenF) {
+        
+        // Proceed only when the future year is greater than the current year
+        if futureYear > currentYear {
+            
+            // Check all the years in the range given
+            for thisYear in 0...(futureYear - currentYear) {
+                
+                // Look for years with 60 as an even divisor
+                if thisYear % 60 == 0 {
+                    
+                    // How many special years?
+                    specialYears += 1
+                    
+                    // Report this year
+                    print("All positions change in year \(thisYear + currentYear)")
+                    
+                }
+                
+            }
+            
+            // Report how unusual this was 
+            let actualPercentage : Float = ( Float(specialYears) / Float((futureYear - currentYear)) ) * Float(100)
+            let leftSide = Int(actualPercentage)
+            var rightSide = ( actualPercentage - Float(leftSide) ) * 10
+            if rightSide - Float(Int(rightSide)) >= 0.5 {
+                rightSide = Float(Int(rightSide) + 1)
+            } else {
+                rightSide = Float(Int(rightSide))
+            }
+            print("\(leftSide).\(Int(rightSide))%")
+            
+        }
+        
+    }
+    
+}
+
+
+
+
